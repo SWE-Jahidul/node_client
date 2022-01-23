769fa92981dcfa9302b4524db02f84ef75eebb0f
jahidul <programmerjahid162@gmail.com>
2022-01-23 14:29:17 +0600

    test

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 // const Generator = require('license-key-generator');
-const mysql = require('mysql');
-const bodyparser = require('body-parser');
-
-
+const mysql = require("mysql");
+const bodyparser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -14,25 +12,22 @@ app.use(express.json());
 //add middleware
 app.use(bodyparser.urlencoded({ extended: true }));
 
-
-
 var confiq = {
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"client_portal"
-}
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "client_portal",
+};
 
 var connection = mysql.createConnection(confiq);
-connection.connect(function (error){
-    if(error){
-        console.log("connection fail");
-    }
-    else{
-        console.log("connection success");
-        //insertdata();
-    }
-})
+connection.connect(function (error) {
+  if (error) {
+    console.log("connection fail");
+  } else {
+    console.log("connection success");
+    //insertdata();
+  }
+});
 
 // function insertdata(){
 //     const sqlinsert = "INSERT INTO `users_list`(`Name`, `Email`) VALUES ('iqbal','iqbal@gmail.com')";
@@ -46,46 +41,46 @@ connection.connect(function (error){
 //     })
 // }
 
-app.get("/api",cors(),(req,res)=>{
-    res.json({"users":["iqbal","raju","riaz","sharif"]})
-})
+app.get("/api", cors(), (req, res) => {
+  res.json({ users: ["iqbal", "raju", "riaz", "sharif"] });
+});
 
-app.post("/insert/users",(req,res)=>{
-    const Name = req.body.Name;
-    const Email = req.body.Email;
+app.post("/register", (req, res) => {
+  const Name = req.body.Name;
+  const Email = req.body.Email;
 
-    //insert query
-    const sqlinsert = "INSERT INTO `registration`(`email`, `password`) VALUES (?,?)";
-    connection.query(sqlinsert,[Name,Email],function(error,results){
-        if(error){
-            console.log(error);
-        }
-    })
-})
+  //insert query
+  const sqlinsert =
+    "INSERT INTO `registration`(`email`, `password`) VALUES (?,?)";
+  connection.query(sqlinsert, [Name, Email], function (error, results) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
 
-app.get("/insert/users/data",(req,res)=>{
-    const selectquery = "SELECT `email`, `password` FROM `registration`";
-    connection.query(selectquery,function(error,results){
-        if(error){
-            console.log(error);
-        }
-        else{
-            console.log(results);
-        }
-    })
-})
+app.get("/register", (req, res) => {
+  const selectquery = "SELECT `email`, `password` FROM `registration`";
+  connection.query(selectquery, function (err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      data,
+      message: "User lists retrieved successfully",
+    });
+  });
+});
 
-app.get("/",(req,res)=>{
-    res.send("hello world");
-   
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.post("/users",(req,res)=>{
-    let {email,id} = req.body
-    console.log(email);
-    console.log(id);
-})
+app.post("/users", (req, res) => {
+  let { email, id } = req.body;
+  console.log(email);
+  console.log(id);
+});
 
-app.listen(port,()=>{
-    console.log("Server started on port",port);
-})
+app.listen(port, () => {
+  console.log("Server started on port", port);
+});
